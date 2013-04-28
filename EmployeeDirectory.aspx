c56430d74c6cd1d9ece66b07cedb1ddb8c20ab04
Kobe <kobi@imoon.co.il>
2013-04-28 14:58:16 +0300

    Added Employee Directory: displayes all employees on DB. Still BUGGY on web.config

---
+++ EmployeeDirectory.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="Dorknozzle Employee Directory" Language="C#" MasterPageFile="~/Dorknozzle.master" AutoEventWireup="true" CodeFile="EmployeeDirectory.aspx.cs" Inherits="EmployeeDirectory" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>Employee Directory</h1>
+    <asp:Repeater ID="employeesRepeater" runat="server">
+        <ItemTemplate>
+            Employee ID: <strong><%#Eval("EmployeeID") %></strong><br />
+            Name: <strong><%#Eval("Name") %></strong><br />
+            Username: <strong><%#Eval("Username") %></strong><br />
+        </ItemTemplate>
+        <SeparatorTemplate>
+            <hr />
+        </SeparatorTemplate>
+    </asp:Repeater>
+</asp:Content>
+
---
+++ EmployeeDirectory.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class EmployeeDirectory : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConnection conn;
+        SqlCommand comm;
+        SqlDataReader reader;
+        string connectionString =
+            ConfigurationManager.ConnectionStrings[
+            "Dorknozzle"].connectionString;
+        conn = new SqlConnection(connectionString);
+        comm = new SqlCommand(
+            "SELECT EmployeeID, Name, Username FROM EMployees", conn);
+        try
+        {
+            conn.Open();
+            reader = comm.ExecuteReader();
+            employeesRepeater.DataSource = reader;
+            employeesRepeater.DataBind();
+            reader.Close();
+        }
+        //catch
+        //{
+        //    Response.Write("Error retrieving users data from DataBase.");
+        //}
+        finally
+        {
+            conn.Close();
+        }
+    }
+}
